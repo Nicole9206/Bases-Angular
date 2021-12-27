c0d18898dc5239c8566e226fa8ba9bea086dd744
Nicole <nic.latfig@gmail.com>
2021-12-27 17:16:25 -0300

    Angular

--- bases/src/app/app.module.ts
+++ bases/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { HeroeComponent } from './Heroes/heroe/heroe.component';
+import { ListadoComponent } from './heroes/listado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
     ContadorComponent, //control + punto --> se puede elegir opción de importación 
-    HeroeComponent
+    HeroeComponent, ListadoComponent
   ],
   imports: [
     BrowserModule
